7b3566f4c712cb43cc94b34913f4b520615da4d3
vincent.baer <vincent.baer@outscale.com>
2023-10-10 11:32:33 +0000

    Lint python example

--- examples/python/user/__main__.py
+++ examples/python/user/__main__.py
@@ -1,23 +1,44 @@
 """A Python Pulumi program"""
 
-import pulumi
-import pulumi_outscale as outscale 
+import pulumi_outscale as outscale
 
 # How to use and to set a simple example
-deployer = outscale.Keypair(resource_name="deployer", keypair_name="deployer")
-net = outscale.Net(ip_range="10.0.0.0/16", resource_name="demo-pulumi-net")
-subnet = outscale.Subnet(net_id=net.id, ip_range="10.0.0.0/24", subregion_name="eu-west-2a", resource_name="demo-pulumi-subnet")
-igw = outscale.InternetService(resource_name="demo-pulumi-igw")
-igw_link = outscale.InternetServiceLink(resource_name="demo-pulumi-igw-link", internet_service_id=igw.id, net_id=net.id)
-public_ip = outscale.PublicIp(resource_name="demo-pulumi-public-ip")
-route_table = outscale.RouteTable(resource_name="demo-pulumi-route-table", net_id=net.id, )
-route_table_link = outscale.RouteTableLink(resource_name="demo-pulumi-route-table-link", subnet_id=subnet.id, route_table_id=route_table.id)
-route = outscale.Route(resource_name="demo-pulumi-route", destination_ip_range="0.0.0.0/0", gateway_id=igw.id, route_table_id=route_table.id),
-security_group = outscale.SecurityGroup(resource_name="demo-pulumi-sg", description="test security group", net_id=net.id)
-security_group_rule = outscale.SecurityGroupRule(resource_name="demo-pulumi-sgr", flow="Inbound", security_group_id=security_group.id, from_port_range=22, to_port_range=22, ip_protocol="tcp", ip_range="0.0.0.0/0")
-vm = outscale.Vm(resource_name="demo-pulumi-vm", image_id="ami-cd8d714e", vm_type="tinav4.c1r1p2", keypair_name=deployer.keypair_name, security_group_ids=[security_group.id], subnet_id=subnet.id)
-#public_ip_link = outscale.PublicIpLink(resource_name="demo-pulumi-public-ip-link", public_ip=public_ip.id, vm_id=vm.id)
+DEPLOYER = outscale.Keypair(resource_name="deployer",
+                            keypair_name="deployer")
+NET = outscale.Net(ip_range="10.0.0.0/16",
+                   resource_name="demo-pulumi-net")
+SUBNET = outscale.Subnet(net_id=NET.id,
+                         ip_range="10.0.0.0/24",
+                         subregion_name="eu-west-2a",
+                         resource_name="demo-pulumi-subnet")
+IGW = outscale.InternetService(resource_name="demo-pulumi-igw")
+IGW_LINK = outscale.InternetServiceLink(resource_name="demo-pulumi-igw-link",
+                                        internet_service_id=IGW.id,
+                                        net_id=NET.id)
+PUBLIC_IP = outscale.PublicIp(resource_name="demo-pulumi-public-ip")
+ROUTE_TABLE = outscale.RouteTable(resource_name="demo-pulumi-route-table",
+                                  net_id=NET.id, )
+ROUTE_TABLE_LINK = outscale.RouteTableLink(resource_name="demo-pulumi-route-table-link",
+                                           subnet_id=SUBNET.id,
+                                           route_table_id=ROUTE_TABLE.id)
+ROUTE = outscale.Route(resource_name="demo-pulumi-route",
+                       destination_ip_range="0.0.0.0/0",
+                       gateway_id=IGW.id,
+                       route_table_id=ROUTE_TABLE.id)
 
-
-
-
+SECURITY_GROUP = outscale.SecurityGroup(resource_name="demo-pulumi-sg",
+                                        description="test security group",
+                                        net_id=NET.id)
+SECURITY_GROUP_RULE = outscale.SecurityGroupRule(resource_name="demo-pulumi-sgr",
+                                                 flow="Inbound",
+                                                 security_group_id=SECURITY_GROUP.id,
+                                                 from_port_range=22,
+                                                 to_port_range=22,
+                                                 ip_protocol="tcp",
+                                                 ip_range="0.0.0.0/0")
+VM = outscale.Vm(resource_name="demo-pulumi-vm",
+                 image_id="ami-cd8d714e",
+                 vm_type="tinav4.c1r1p2",
+                 keypair_name=DEPLOYER.keypair_name,
+                 security_group_ids=[SECURITY_GROUP.id],
+                 subnet_id=SUBNET.id)
