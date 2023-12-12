f99cd75d95c82902bc922ce4619bd6a1d8e74051
Matthias Gatto <matthias.gatto@outscale.com>
2023-12-12 11:37:08 +0100

    CI python user
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- local_tests.sh
+++ local_tests.sh
@@ -40,6 +40,7 @@ export PATH=$PATH:$HOME/.pulumi/bin
 # a strong password is important
 export PULUMI_CONFIG_PASSPHRASE=wololo
 
+MSG_BASE="Test"
 PULUMICTL_VERSION=v0.0.32
 
 cd $HOME/.pulumi/bin
@@ -48,9 +49,13 @@ tar -xvf pulumictl-${PULUMICTL_VERSION}-linux-amd64.tar.gz
 cd -
 
 make provider build_python
-cd examples/yaml
 
 pulumi login --local
+
+# yaml
+
+cd examples/yaml
+
 pulumi stack init staging
 
 pulumi config set outscale:secretKeyId $OSC_SECRET_KEY
@@ -61,12 +66,43 @@ pulumi config set outscale:endpoints '[{"api": "127.0.0.1:3000"}]'
 
 set -eE
 
-MSG_BASE="Test"
-
 trap "echo [$MSG_BASE yaml pulumi up FAIL]" ERR
 PATH=$PATH:$GOPATH/bin pulumi up --yes
 echo "[$MSG_BASE yaml pulumi up OK]"
 trap "echo [$MSG_BASE yaml pulumi down FAIL]" ERR
 PATH=$PATH:$GOPATH/bin pulumi down --yes
 echo "[$MSG_BASE yaml pulumi down OK]"
-#make example-node-volumes
+
+set +e
+
+# examples python
+
+cd ../python/
+
+python -m venv venv
+source venv/bin/activate
+
+# because I use outscale-pulumi as go path, I can use it here too
+pip install pulumi_cloudinit
+pip install $GOPATH/sdk/python/
+
+cd user/
+
+pulumi stack init staging
+
+pulumi config set outscale:secretKeyId $OSC_SECRET_KEY
+pulumi config set outscale:accessKeyId $OSC_ACCESS_KEY
+pulumi config set outscale:region "eu-west-2"
+pulumi config set outscale:insecure true
+pulumi config set outscale:endpoints '[{"api": "127.0.0.1:3000"}]'
+
+set -eE
+
+trap "echo [$MSG_BASE python user pulumi up FAIL]" ERR
+PATH=$PATH:$GOPATH/bin pulumi up --yes
+echo "[$MSG_BASE python user pulumi up OK]"
+trap "echo [$MSG_BASE python user pulumi down FAIL]" ERR
+PATH=$PATH:$GOPATH/bin pulumi down --yes
+echo "[$MSG_BASE python user pulumi down OK]"
+
+set +e
