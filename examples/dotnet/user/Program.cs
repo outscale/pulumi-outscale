ab25a47d64aff9040cb008b58162d36616d9f437
Matthias Gatto <matthias.gatto@outscale.com>
2024-05-21 18:04:03 +0200

    dotnet example initial commit
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

---
+++ examples/dotnet/user/Program.cs
@@ -0,0 +1,28 @@
+﻿using Pulumi;
+using Pulumi.Outscale;
+
+return await Deployment.RunAsync(() =>
+{
+    var group = new Pulumi.Outscale.SecurityGroup("webserver-secgrp", new SecurityGroupArgs
+    {
+	    Description = "test"
+    });
+
+    var userData = @"
+                #!/bin/bash
+                echo ""Hello, World!"" > index.html
+                nohup python -m SimpleHTTPServer 80 &
+                ";
+
+    var server = new Pulumi.Outscale.Vm("webserver-www", new VmArgs
+    {
+        // t2.micro is available in the AWS free tier
+        VmType = "t2.micro",
+        SecurityGroupIds = new[] {
+		group.Id
+	}, // reference the security group resource above
+        UserData = userData,
+        ImageId = "ami-cd8d714e",
+    });
+
+});
